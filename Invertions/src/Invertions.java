23a9da610c74223c9247b5f919d3e684fee5d0db
evteevm <mevteev@gmail.com>
2015-07-01 17:41:21 +0400

    Sort with invertions counting

--- Invertions/src/Invertions.java
+++ Invertions/src/Invertions.java
@@ -2,9 +2,12 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class Invertions {
+    
+    public static long inversions = 0;
+    
 
     public static void main(String[] args) throws Exception {
-        int[] arr = new int[10];
+        int[] arr = new int[100000];
         int i = 0;
         if (args.length == 0 ) {
             throw new java.lang.Exception("Specify file name");
@@ -28,10 +31,12 @@ public class Invertions {
         
         int[] res = mergeSort(arr);
         
+        /*
         for (i = 0; i < res.length; i++) {
             System.out.println(res[i]);
         }
-        
+        */
+        System.out.println(inversions);
     }
     
     
@@ -39,11 +44,15 @@ public class Invertions {
         int n = a.length + b.length;
         int[] res = new int[n];
         
+        
+        
         for (int k = 0, i = 0, j = 0; k < n; k++) {
             if (i < a.length && (j >= b.length || a[i] < b[j])) {
                 res[k] = a[i++];
             } else {
                 res[k] = b[j++];
+                
+                inversions += (a.length - i);
             }
         }
         
